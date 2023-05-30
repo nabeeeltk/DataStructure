7821b06daa6d1e146b1e7419193b18cad2b670b3
nabeeeltk <nabeelmuhammed406@gmail.com>
2023-05-30 15:24:52 +0530

    add queue

---
+++ DATASTRUCTURE/Queue.dart
@@ -0,0 +1,44 @@
+class Queue {
+  late List<dynamic> _data;
+
+  Queue() {
+    _data = [];
+  }
+
+  void enqueue(dynamic value) {
+    _data.add(value);
+  }
+
+  dynamic dequeue() {
+    if (_data.isEmpty) {
+      throw Exception('Queue is empty');
+    }
+    return _data.removeAt(0);
+  }
+
+  dynamic front() {
+    if (_data.isEmpty) {
+      throw Exception('Queue is empty');
+    }
+    return _data.first;
+  }
+
+  dynamic rear() {
+    if (_data.isEmpty) {
+      throw Exception('Queue is empty');
+    }
+    return _data.last;
+  }
+}
+
+void main() {
+  var queue = Queue();
+  queue.enqueue(20);
+  queue.enqueue(10);
+  queue.enqueue(30);
+  queue.enqueue(5);
+  queue.enqueue(15);
+  queue.dequeue();
+  queue.rear();
+  print(queue._data);
+}

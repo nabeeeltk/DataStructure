1d82d995350db87cd4c4203be257f4117f842ce7
nabeeeltk <nabeelmuhammed406@gmail.com>
2023-06-01 11:52:10 +0530

    stack

--- DATASTRUCTURE/Queue.dart
+++ DATASTRUCTURE/Queue.dart
@@ -1,44 +1,83 @@
+// class Queue {
+//   late List<dynamic> _data;
+
+//   Queue() {
+//     _data = [];
+//   }
+
+//   void enqueue(dynamic value) {
+//     _data.add(value);
+//   }
+
+//   dynamic dequeue() {
+//     if (_data.isEmpty) {
+//       throw Exception('Queue is empty');
+//     }
+//     return _data.removeAt(0);
+//   }
+
+//   dynamic front() {
+//     if (_data.isEmpty) {
+//       throw Exception('Queue is empty');
+//     }
+//     return _data.first;
+//   }
+
+//   dynamic rear() {
+//     if (_data.isEmpty) {
+//       throw Exception('Queue is empty');
+//     }
+//     return _data.last;
+//   }
+// }
+
+// void main() {
+//   var queue = Queue();
+//   queue.enqueue(20);
+//   queue.enqueue(10);
+//   queue.enqueue(30);
+//   queue.enqueue(5);
+//   queue.enqueue(15);
+//   queue.dequeue();
+//   queue.rear();
+//   print(queue._data);
+// }
 class Queue {
   late List<dynamic> _data;
 
   Queue() {
     _data = [];
   }
-
   void enqueue(dynamic value) {
     _data.add(value);
   }
 
-  dynamic dequeue() {
+  void dequeue() {
     if (_data.isEmpty) {
-      throw Exception('Queue is empty');
+      throw Exception('no data');
     }
     return _data.removeAt(0);
   }
 
   dynamic front() {
     if (_data.isEmpty) {
-      throw Exception('Queue is empty');
+      throw Exception('no data');
     }
     return _data.first;
   }
 
   dynamic rear() {
     if (_data.isEmpty) {
-      throw Exception('Queue is empty');
+      throw Exception('no data ');
     }
     return _data.last;
   }
 }
 
-void main() {
-  var queue = Queue();
-  queue.enqueue(20);
+void main(List<String> args) {
+  Queue queue = Queue();
+
   queue.enqueue(10);
-  queue.enqueue(30);
-  queue.enqueue(5);
-  queue.enqueue(15);
-  queue.dequeue();
-  queue.rear();
+  queue.enqueue(20);
   print(queue._data);
 }
--- DATASTRUCTURE/Stack.dart
+++ DATASTRUCTURE/Stack.dart
@@ -1,3 +1,46 @@
+// class Stack {
+//   late List<dynamic> _data;
+
+//   Stack() {
+//     _data = [];
+//   }
+
+//  void  push(dynamic item) {
+//     _data.add(item);
+//   }
+
+//   dynamic pop() {
+//     if (isEmpty()) {
+//       throw Exception('Stack is empty');
+//     }
+//     return _data.removeLast();
+//   }
+
+//   dynamic peek() {
+//     if (isEmpty()) {
+//       throw Exception('Stack is empty');
+//     }
+//     return _data.last;
+//   }
+
+//   bool isEmpty() {
+//     return _data.isEmpty;
+//   }
+
+//   int size() {
+//     return _data.length;
+//   }
+// }
+// void main() {
+//   var stack = Stack();
+//   stack.push(1);
+//   stack.push(2);
+//   stack.push(3);
+//   stack.pop();
+//   stack.size();
+//  print(stack._data);
+
+// }
 class Stack {
   late List<dynamic> _data;
 
@@ -5,40 +48,36 @@ class Stack {
     _data = [];
   }
 
- void  push(dynamic item) {
-    _data.add(item);
+  void push(dynamic value) {
+    _data.add(value);
   }
 
   dynamic pop() {
-    if (isEmpty()) {
-      throw Exception('Stack is empty');
+    if (_data.isEmpty) {
+      throw Exception('no data ');
     }
     return _data.removeLast();
   }
 
   dynamic peek() {
-    if (isEmpty()) {
-      throw Exception('Stack is empty');
+    if (_data.isEmpty) {
+      throw Exception("no data ");
     }
     return _data.last;
   }
 
   bool isEmpty() {
-    return _data.isEmpty;
+    return isEmpty();
   }
 
   int size() {
     return _data.length;
   }
 }
-void main() {
-  var stack = Stack();
-  stack.push(1);
-  stack.push(2);
-  stack.push(3);
-  stack.pop();
-  stack.size();
- print(stack._data);
-  
 
+void main(List<String> args) {
+  Stack stack = Stack();
+  stack.push(20);
+  stack.push(10);
+  print(stack._data);
 }
